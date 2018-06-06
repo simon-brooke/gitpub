04c29dab0a234c47408344c61ea0b8c92cffdf8b
Simon Brooke <simon@journeyman.cc>
2018-06-06 12:24:00 +0100

    Commit of initial leiningen template code

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gitpub
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gitpub/core_test.clj
@@ -0,0 +1,7 @@
+(ns gitpub.core-test
+  (:require [clojure.test :refer :all]
+            [gitpub.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
